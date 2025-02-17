3090582bb4ef7cc99e3c6c0649339a28fe18d6dd
geotilageamb <geoti.lageamb@ufpr.br>
2025-02-17 14:57:45 -0300

    Update c_dashboard_docs_recebidos.py

--- c_dashboard_docs_recebidos.py
+++ c_dashboard_docs_recebidos.py
@@ -13,7 +13,7 @@ def load_data():
     return pd.read_excel('03_contDocsRecebidos.xlsx')
 
 def show_dashboard():
-    st.header("2.2.1.1 Documentos recebidos de assentados e NMRFs")
+    st.header("Produto 2.2.1.1 da meta 2.2 Documentos recebidos de assentados e NMRFs")
     df_docs = load_data()
 
     # Remover caracteres especiais para facilitar a manipulação
